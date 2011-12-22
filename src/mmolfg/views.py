638d740675a66cdde9b7085acf9b16b106f43c9d
Deryck Hodge <deryck@devurandom.org>
2011-12-22 16:15:43 -0600

    Show that context processors are working.

--- src/mmolfg/views.py
+++ src/mmolfg/views.py
@@ -6,5 +6,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    """The site's main index page."""
+    """The site's main index/home page."""
     return render(request, 'home.html', {})
