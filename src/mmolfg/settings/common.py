ec6daec4e55b656102d3d723bfdbc497f5dd13d7
Deryck Hodge <deryck@devurandom.org>
2012-01-04 20:54:15 -0600

    Get registration form working.

--- src/mmolfg/settings/common.py
+++ src/mmolfg/settings/common.py
@@ -14,6 +14,19 @@ DATABASES = {
     }
 }
 
+# Resistration settings
+ACCOUNT_ACTIVATION_DAYS = 7
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfResponseMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+)
+
 ROOT_URLCONF = 'mmolfg.urls'
 
 INSTALLED_APPS = (
@@ -21,7 +34,9 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.humanize',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'registration',
     'mmolfg',
 )
