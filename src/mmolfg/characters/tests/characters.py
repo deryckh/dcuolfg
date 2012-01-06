33dc04d99ce00cae475bf058e609b082805ae73d
Deryck Hodge <deryck@devurandom.org>
2012-01-06 13:31:56 -0600

    First bits of a real Character object test.

--- src/mmolfg/characters/tests/characters.py
+++ src/mmolfg/characters/tests/characters.py
@@ -1,11 +1,22 @@
 """
-Starter test.
+Tests for Character objects in mmolfg.
 """
 
 import unittest
 
-class TestBaseTest(unittest.TestCase):
+from mmolfg.characters.models import Character
 
-    def test_first_test(self):
-        self.assertTrue(True)
 
+class TestCharacterAttributes(unittest.TestCase):
+    """Tests to ensure expected attributes for Character objects.
+
+    Do not call save or use Character.objects.create (which calls
+    save).  This avoids saving to DB, since we're just checking
+    the Character object's API.
+    """
+
+    def test_character_name(self):
+        """Each Character instance should have a name attribute."""
+        name = 'superdoopertoon'
+        toon = Character(name=name)
+        self.assertEqual(name, toon.name)
