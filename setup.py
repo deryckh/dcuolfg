1d72743876bafc3ee4f36287c1969e68eee4fa5f
Deryck Hodge <deryck@devurandom.org>
2012-03-05 12:20:39 -0600

    Remove TODO, add couple missing header copyrights, and begin mmolfg --> dcuolfg rename.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
+#
+# Copyright 2012 Deryck Hodge.  This software is licensed under the
+# GNU Lesser General Public License version 3 (see the file LICENSE).
+
 
 from setuptools import setup, find_packages
 
 __version__ = '0.1'
 
 setup(
-    name = 'mmolfg',
+    name = 'dcuolfg',
     version = __version__,
     packages = find_packages('src'),
     package_dir = {'': 'src'},
